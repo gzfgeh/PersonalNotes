da20c1cd23f96294b84fbd6fd755b3789ef80854
gzfgeh <gzfgeh@qq.com>
2015-03-22 15:10:56 +0800

    add default_image.

--- PersonalNotes/src/com/gzfgeh/set/PersonalInfo.java
+++ PersonalNotes/src/com/gzfgeh/set/PersonalInfo.java
@@ -1,23 +1,33 @@
 package com.gzfgeh.set;
 
+import java.io.File;
+
 import com.gzfgeh.personalnote.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.TextView;
 
 public class PersonalInfo extends Activity implements OnClickListener {
+	private static final String FILE_PATH = Environment.getExternalStorageDirectory() + 
+			File.separator + "PersonalNote" + File.separator;
 	private View titleRightView, titleCenterView, titleLeftView;
 	private TextView titleLeftTextView;
+	private View headSelect;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.personal_info);
+		
+		File fold = new File(FILE_PATH);
+		if (!fold.exists())
+			fold.mkdirs();
 		
 		titleRightView = findViewById(R.id.title_right);
 		titleRightView.setVisibility(View.INVISIBLE);
@@ -27,11 +37,24 @@ public class PersonalInfo extends Activity implements OnClickListener {
 		titleLeftTextView.setText(R.string.personal_info);
 		titleLeftView = findViewById(R.id.title_left);
 		titleLeftView.setOnClickListener(this);
+		headSelect = findViewById(R.id.head_select);
+		headSelect.setOnClickListener(this);
 	}
 	@Override
 	public void onClick(View view) {
 		// TODO Auto-generated method stub
-		finish();
+		switch (view.getId()) {
+		case R.id.head_select:
+			
+			break;
+			
+		case R.id.title_left:
+			finish();
+			break;
+			
+		default:
+			break;
+		}
 	}
 	
 }
