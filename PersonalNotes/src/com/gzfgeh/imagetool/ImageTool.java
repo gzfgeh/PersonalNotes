cb85fd87edf9e8e87cf12882c6b421007067bb9d
gzfgeh <gzfgeh@sina.com>
2015-03-24 23:22:45 +0800

    add get_photo function.

--- PersonalNotes/src/com/gzfgeh/imagetool/ImageTool.java
+++ PersonalNotes/src/com/gzfgeh/imagetool/ImageTool.java
@@ -1,13 +1,89 @@
 package com.gzfgeh.imagetool;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.gzfgeh.set.PersonalInfo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 
 public class ImageTool {
+	
+	private ImageTool(){
+		throw new AssertionError();
+	};
+	
 	public static Bitmap setSDImageView(String filePath){
 		BitmapFactory.Options options = new BitmapFactory.Options();
 		options.inSampleSize = 1;
 		Bitmap bm = BitmapFactory.decodeFile(filePath, options);
 		return bm;
+	}
+	
+	public static String getPhotoPathByLocalUri(Context context, Intent data) {
+        Uri selectedImage = data.getData();
+        String[] filePathColumn = { MediaStore.Images.Media.DATA };
+        Cursor cursor = context.getContentResolver().query(selectedImage,
+                filePathColumn, null, null, null);
+        cursor.moveToFirst();
+        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+        String picturePath = cursor.getString(columnIndex);
+        cursor.close();
+        return picturePath;
+    }
+	
+	public static void startCrop(Activity activity, String imagePath,
+			int requestCode) {
+		File f = new File(imagePath);
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setDataAndType(Uri.fromFile(f), "image/*");
+		intent.putExtra("crop", "true");
+		intent.putExtra("aspectX", 1);
+		intent.putExtra("aspectY", 1);
+		intent.putExtra("outputX", 200);
+		intent.putExtra("outputY", 150);
+		intent.putExtra("scale", true);
+		intent.putExtra("return-data", true); // 设置true才能有返回
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, imagePath);
+		intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+		intent.putExtra("noFaceDetection", true); // no face detection
+		activity.startActivityForResult(intent, requestCode);
+	}
+	
+	public static void saveBitmapToSDCard(Bitmap bitmap){
+		File outputFile = new File(PersonalInfo.FILE_PATH + PersonalInfo.FILE_NAME);
+		try {
+			if (outputFile.exists())
+				outputFile.delete();
+			outputFile.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        FileOutputStream fOut = null;
+        try {
+                fOut = new FileOutputStream(outputFile);
+        } catch (FileNotFoundException e) {
+                e.printStackTrace();
+        }
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+        try {
+                fOut.flush();
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
+        try {
+                fOut.close();
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
 	}
 }
--- PersonalNotes/src/com/gzfgeh/set/PersonalInfo.java
+++ PersonalNotes/src/com/gzfgeh/set/PersonalInfo.java
@@ -10,6 +10,7 @@ import com.gzfgeh.imagetool.ImageTool;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -21,11 +22,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PersonalInfo extends Activity implements OnClickListener {
-	private static final String FILE_PATH = Environment.getExternalStorageDirectory() + 
+	public static final String FILE_PATH = Environment.getExternalStorageDirectory() + 
 			File.separator + "PersonalNote" + File.separator;
-	private static final String FILE_NAME = "image.jpg";
-	private static final int REQUEST_CODE = 1;
-	private static final int CROP_PHOTO = 2;
+	public static final String FILE_NAME = "image.jpg";
+	private static final int FROM_CAPTURE = 1;
+	private static final int FROM_GALLERY = 2;
+	private static final int CROP_PHOTO = 3;
 	
 	private View titleRightView, titleCenterView, titleLeftView;
 	private TextView titleLeftTextView;
@@ -35,7 +37,7 @@ public class PersonalInfo extends Activity implements OnClickListener {
 	private Effectstype effect;
 	private NiftyDialogBuilder dialogBuilder;
 	
-	private Intent openCameraIntent;
+	private Intent getImageIntent;
 	private File outputFile;
 	private Uri imageUri;
 	
@@ -63,7 +65,7 @@ public class PersonalInfo extends Activity implements OnClickListener {
 		headImageView = (ImageView) findViewById(R.id.head_image_view);
 		
 		outputFile = new File(FILE_PATH + FILE_NAME);
-		if (!outputFile.exists())
+		if (outputFile.length() == 0 || !outputFile.exists())
 			headImageView.setImageResource(R.drawable.default_image);
 		else 
 			headImageView.setImageBitmap(ImageTool.setSDImageView(FILE_PATH + FILE_NAME));
@@ -106,53 +108,57 @@ public class PersonalInfo extends Activity implements OnClickListener {
 	public void onSelectPhoto(View view){
 		switch (view.getId()) {
 		case R.id.make_photo:
-			openCameraIntent = new Intent("android.media.action.IMAGE_CAPTURE");
 			try {
 				if (outputFile.exists())
 					outputFile.delete();
 				outputFile.createNewFile();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			imageUri = Uri.fromFile(outputFile);
-			openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-			startActivityForResult(openCameraIntent, REQUEST_CODE);
-			dialogBuilder.dismiss();
+			getImageIntent = new Intent("android.media.action.IMAGE_CAPTURE");
+			getImageIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+			startActivityForResult(getImageIntent, FROM_CAPTURE);
 			break;
 			
 		case R.id.get_photo:
-			dialogBuilder.dismiss();
+			getImageIntent = new Intent(Intent.ACTION_PICK, null);
+			getImageIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+					"image/*");
+			startActivityForResult(getImageIntent, FROM_GALLERY);
 			break;
 
 		default:
 			break;
 		}
+		dialogBuilder.dismiss();
 	}
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		if (resultCode == RESULT_OK){
 			switch (requestCode) {
-			case REQUEST_CODE:
-				Intent intent = new Intent("com.android.camera.action.CROP");
-				intent.setDataAndType(imageUri, "image/*");
-				intent.putExtra("scale", true);
-				intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-				startActivityForResult(intent, CROP_PHOTO);
+			case FROM_CAPTURE:
+				ImageTool.startCrop(PersonalInfo.this, outputFile.getAbsolutePath(), CROP_PHOTO);
 				break;
+				
+			case FROM_GALLERY:
+				if (data != null) {
+					ImageTool.startCrop(PersonalInfo.this, ImageTool.getPhotoPathByLocalUri(this, data), CROP_PHOTO);
+				}
+				
+				break;
+				
 			case CROP_PHOTO:
-				headImageView.setImageBitmap(ImageTool.setSDImageView(FILE_PATH + FILE_NAME));
-//				try {
-//					Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
-//					headImageView.setImageBitmap(bitmap);
-//				} catch (FileNotFoundException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
+				if (data != null) {
+					Bitmap bitmap = data.getParcelableExtra("data");
+					ImageTool.saveBitmapToSDCard(bitmap);
+					headImageView.setImageBitmap(bitmap);
+				}
 				break;
 				
 			default:
+				headImageView.setImageResource(R.drawable.default_image);
 				break;
 			}
 		}
